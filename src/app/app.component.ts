d0ee36e04a0b50fb0e2ff650cf5d0425f4fbfcb6
Vincent-lebrun <lebrunvincent12@gmail.com>
2022-05-19 14:55:31 +0200

    Pass my first props in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'testAngular';
+  title = 'Selfie à wookie';
+  subTitle = 'Voyons ce que celà fait ';
 }
