29b21341189b7c799998eb3b430d5d43564a0a34
Vincent-lebrun <lebrunvincent12@gmail.com>
2022-05-20 14:14:12 +0200

    Declare module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuGeneralComponent } from './shared/component/menu-general/menu-general.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuGeneralComponent
   ],
   imports: [
     BrowserModule
