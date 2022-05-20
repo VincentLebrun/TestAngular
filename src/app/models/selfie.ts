0dc0200602a503781f8f253d5dbd981ffce17427
Vincent-lebrun <lebrunvincent12@gmail.com>
2022-05-20 11:04:08 +0200

    add wookie and selfie

---
+++ src/app/models/selfie.ts
@@ -0,0 +1,15 @@
+import { Wookie } from "./wookie";
+
+/**
+ * Class for wookie selfiue
+ */
+export class Selfie {
+   
+    image : string | undefined;
+    wookie : Wookie;
+    
+    constructor(){
+        this.wookie = new Wookie();
+    }
+    
+}
