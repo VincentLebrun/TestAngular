efb568b533fbe055634026f21f2788f317e26fb5
Vincent-lebrun <lebrunvincent12@gmail.com>
2022-05-23 11:50:43 +0200

    Add new component one selfie and now selfie list contains one selfie separated it's more readable

---
+++ src/app/features/selfies/un-selfie-readonly/un-selfie-readonly.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-un-selfie-readonly',
+  templateUrl: './un-selfie-readonly.component.html',
+  styleUrls: ['./un-selfie-readonly.component.css']
+})
+export class UnSelfieReadonlyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
