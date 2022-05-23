cbb8b74b427d569b61c836e00c1c80767d9f8b3b
Vincent-lebrun <lebrunvincent12@gmail.com>
2022-05-23 11:53:18 +0200

    Modified the place to call the ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { MenuGeneralComponent } from './shared/component/menu-general/menu-general.component';
 import { FooterComponent } from './shared/component/footer/footer.component';
 import { SelfieListComponent } from './features/selfies/selfie-list/selfie-list.component';
+import { UnSelfieReadonlyComponent } from './features/selfies/un-selfie-readonly/un-selfie-readonly.component';
 
 
 @NgModule({
@@ -13,6 +14,7 @@ import { SelfieListComponent } from './features/selfies/selfie-list/selfie-list.
     MenuGeneralComponent,
     FooterComponent,
     SelfieListComponent,
+    UnSelfieReadonlyComponent,
 
   ],
   imports: [
