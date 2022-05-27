3ee5eaf02799b6f393d57a301906741c60165538
Vincent-lebrun <lebrunvincent12@gmail.com>
2022-05-27 12:18:53 +0200

    bug

--- src/app/models/selfie.ts
+++ src/app/models/selfie.ts
@@ -4,14 +4,14 @@ import { Wookie } from "./wookie";
  * Class for wookie selfie
  */
 export class Selfie {
-   
-    image?: string ;
-    wookie : Wookie;
-    title : string;
-    
-    constructor(){
+
+    image?: string;
+    wookie: Wookie;
+    title: string;
+
+    constructor() {
         this.wookie = new Wookie();
     }
-    
-    
+
+
 }
