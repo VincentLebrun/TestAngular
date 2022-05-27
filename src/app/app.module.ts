f8c1b9cb5f1074d9172f015e60216b985af54e6a
Vincent-lebrun <lebrunvincent12@gmail.com>
2022-05-27 15:05:20 +0200

    uncomment Logger service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
-import { MenuGeneralComponent } from './shared/component/menu-general/menu-general.component';
-import { FooterComponent } from './shared/component/footer/footer.component';
 import { SelfieListComponent } from './features/selfies/selfie-list/selfie-list.component';
 import { UnSelfieReadonlyComponent } from './features/selfies/un-selfie-readonly/un-selfie-readonly.component';
+import { FooterComponent } from './shared/component/footer/footer.component';
+import { MenuGeneralComponent } from './shared/component/menu-general/menu-general.component';
 import { LoggerService } from './shared/services/logger/logger.service';
 
 
