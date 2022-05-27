aa3fbdc9669766142de2b141388664f9c6fcd846
Vincent-lebrun <lebrunvincent12@gmail.com>
2022-05-27 15:06:20 +0200

    comment function and call api but spring security stuck

--- src/app/shared/services/selfies/selfie.service.ts
+++ src/app/shared/services/selfies/selfie.service.ts
@@ -1,7 +1,10 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { interval, map, Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Selfie } from 'src/app/models/selfie';
 import { Wookie } from 'src/app/models/wookie';
+import { environment } from 'src/environments/environment';
+import { LoggerService } from '../logger/logger.service';
 
 /**
  * injection of service
@@ -11,7 +14,7 @@ import { Wookie } from 'src/app/models/wookie';
 })
 export class SelfieService {
 
-  constructor() { }
+  constructor(private _LoggerService: LoggerService, private _httpClient: HttpClient) { }
   /**
    * 
    * @returns return the total list of selfie 
@@ -37,19 +40,20 @@ export class SelfieService {
 
   }
   getAll_asObservable(): Observable<Selfie[]> {
-    const myTab = this.getAll();
-    // return of(myTab);
-    return interval(1000).pipe(
-      map(int => [
-        {
-          image: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fanimal.memozee.com%2FArchOLD-7%2F1193645414.jpg&f=1&nofb=1",
-          title: 'Pigeon ' + int,
-          wookie: {
-            name: 'Pigeon voyager ',
-            selfies: []
-          }
-        }
-      ])
-    );
+    // const myTab = this.getAll();
+    // // return of(myTab);
+    // return interval(1000).pipe(
+    //   map(int => [
+    //     {
+    //       image: "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fanimal.memozee.com%2FArchOLD-7%2F1193645414.jpg&f=1&nofb=1",
+    //       title: 'Pigeon ' + int,
+    //       wookie: {
+    //         name: 'Pigeon voyager ',
+    //         selfies: []
+    //       }
+    //     }
+    //   ])
+    // );
+    return this._httpClient.get<Selfie[]>(environment.apis.selfies.url);
   }
 }
